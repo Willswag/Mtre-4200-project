fbb76ae3d5118bf1ae9aa0b4dcbe0a8351bd227a
willswag <wswaguespack403@gmail.com>
2020-04-15 12:27:39 -0400

    worked on code still not perfect

--- mtre4200projectpt1.m
+++ mtre4200projectpt1.m
@@ -11,23 +11,23 @@
 coms = [0, 0, 0, 0, 0, 0]; 
 
 %the distance between the previous x-axis and the current x-axis, along the previous z-axis.
-d = {.5,1.5+coms(2),2,4+coms(5),.5,0}; 
+d = {0,1+coms(2),.5,0,2+coms(5),0,0}; 
 
 %the angle around the z-axis between the previous x-axis and the current x-axis.
-thet = {coms(1), 0, coms(3), coms(4), 0, coms(6)}; 
+thet = {coms(1), 0, coms(3), coms(4), 0, coms(6),-pi/2}; 
 
 
 %the length of the common normal, which is the distance between the previous z-axis and the current z-axis
-a = {0, 0, 0, 0, 0, 0, 0, };
+a = {.5, 0, 0, 2, 0, 2,.5 };
 
 %the angle around the common normal to between the previous z-axis and current z-axis.
-alph = {pi/2, 0, pi/2, pi/2, 0, -pi/2};
+alph = {-pi/2,0, pi/2, pi/2, 0, -pi/2,0};
 
 %storage cell for each H matrix of the robot
 h = cell(6,1);
 
 %loop through the joints anc calculate kinematics
-for i = 1:6
+for i = 1:7
     
     %pull the proper params from the param cells
     dc = cell2mat(d(i));
@@ -36,13 +36,13 @@ for i = 1:6
     alc =  cell2mat(alph(i));
 
     %calc the kinematics of the robotic arm
-    t = [cos(thc), -sin(thc)*cos(alc), sin(thc)*sin(alc), ac*cos(thc);
-            sin(thc), cos(thc)*sin(alc), -cos(thc)*sin(alc), ac*cos(thc);
-            0, sin(alc), cos(alc), dc;
-            0, 0, 0, 1];
+    t = [cos(thc), -sin(thc)*cos(alc),  sin(thc)*sin(alc), ac*cos(thc);
+         sin(thc),  cos(thc)*cos(alc), -cos(thc)*sin(alc), ac*sin(thc);
+         0,         sin(alc),           cos(alc),          dc;
+         0,         0,                  0,                 1];
         
     %store kinematic matrixes in cell     
-    h(i) = mat2cell(t,4,4)
+    h(i) = mat2cell(t,4,4);
         
 end
 
@@ -50,7 +50,7 @@ end
 r = eye(4:4);
 
 %loop through the h matrix and calc overall model
-for i = 1:6
+for i = 1:7
     
     r = r*cell2mat(h(i))
 end
